060d8ce5698dfa2dc42b7bf434dc644a26b65b45
Sudhir Meena <sureshmeena512@gmail.com>
2022-04-28 16:55:35 +0530

    assert remove

--- calculations2/calculations_test.go
+++ calculations2/calculations_test.go
@@ -2,23 +2,37 @@ package calculations
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestSI(t *testing.T) {
-	t1 := calculateSI(42000.00, 2.5, 5.00)
-	assert.Equal(t, "5250.00", t1, "they should be equal")
+	got := calculateSI(42000.00, 2.5, 5.00)
+	want := "5250.00"
 
-	t2 := calculateSI(36000.00, 3.75, 5)
-	assert.Equal(t, "6750.00", t2, "they should be equal")
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "5250.00")
+	}
+
+	got = calculateSI(36000.00, 3.75, 5)
+	want = "6750.00"
+
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "6750.00")
+	}
 }
 
 func TestCI(t *testing.T) {
-	// compounded annually
-	t1 := calculateCI(42000.00, 2.5, 5.00)
-	assert.Equal(t, "5519.14", t1, "they should be equal")
 
-	t2 := calculateCI(36000.00, 3.75, 5)
-	assert.Equal(t, "7275.59", t2, "they should be equal")
+	got := calculateCI(42000.00, 2.5, 5.00)
+	want := "5519.14"
+
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "5519.14")
+	}
+
+	got = calculateCI(36000.00, 3.75, 5)
+	want = "7275.59"
+
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "7275.59")
+	}
 }
--- tests/main_test.go
+++ tests/main_test.go
@@ -1,37 +1,22 @@
 package tests
 
 import (
-	"strconv"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestFacto(t *testing.T) {
-	t1 := 5 * 4 * 3 * 2 * 1
-	t2 := facto(5)
-	assert.Equal(t, t1, t2, "they should be equal")
 
-	f := facto(-1)
-	assert.Equal(t, 1, f, "factorial of -1 should be 0")
-}
+	got := facto(5)
+	want := 120
 
-//I'm testing the Atoi(string) function from the package strconv
-func TestAtoi(t *testing.T) {
-	tests := map[string]struct {
-		input  string
-		output int
-		err    error
-	}{
-		"successful conversion": {input: "1", output: 1, err: nil},
-		"invalid integer":       {input: "a text", output: 0, err: &strconv.NumError{}},
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, 120)
 	}
 
-	for testName, test := range tests {
-		t.Logf("Running test case %s", testName)
+	got = facto(0)
+	want = 1
 
-		output, err := strconv.Atoi(test.input)
-		assert.IsType(t, test.err, err)
-		assert.Equal(t, test.output, output)
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, 1)
 	}
 }
